aa9c5802ef4364b6c52a4f0788163c05637b8ade
Lorenzo Gagliani <lorenz_g@hotmail.it>
2019-10-24 10:36:12 +0200

    initial base project

---
+++ src/main/java/com/bank/service/moneytransfer/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.bank.service.moneytransfer.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping(method = RequestMethod.GET, value = "/test")
+    public String test() {
+        return "<b>Ecco un test</b>";
+    }
+}
