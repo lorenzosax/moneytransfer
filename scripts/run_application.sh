b254ee36f3734b3027aea91eae5f2b87a17eeb6e
Lorenzo Gagliani <lorenz_g@hotmail.it>
2019-10-29 22:35:36 +0100

    test with nohup command

--- scripts/run_application.sh
+++ scripts/run_application.sh
@@ -1,2 +1,1 @@
-ssh root@206.81.16.40 "mkdir /opt/apps/test1 && cd /opt/apps/money-transfer && java -jar -Dspring.profiles.active=prod moneytransfer-*.jar &"
-exit 0
+ssh root@206.81.16.40 "mkdir /opt/apps/test1 && cd /opt/apps/money-transfer && nohup java -jar -Dspring.profiles.active=prod moneytransfer-*.jar &"
