a3bf81f178a5a8185e2bd2d009e2df95784a2b56
Lorenzo Gagliani <lorenz_g@hotmail.it>
2019-10-29 20:29:16 +0100

    try to run application in correct way

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,2 @@
-rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/build/libs/ root@206.81.16.40:/opt/apps/money-transfer
 ssh root@206.81.16.40 "cd /opt/apps/money-transfer && java -jar -Dspring.profiles.active=prod moneytransfer-*.jar &"
 exit 0
