51ce851a404147dba5d371d37134e3e9b6f55e04
Lorenzo Gagliani <lorenz_g@hotmail.it>
2019-10-29 22:00:20 +0100

    test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,2 @@
-ssh root@206.81.16.40 "cd /opt/apps/money-transfer && java -jar -Dspring.profiles.active=prod moneytransfer-*.jar &"
+ssh root@206.81.16.40 "mkdir /opt/apps/test1 && cd /opt/apps/money-transfer && java -jar -Dspring.profiles.active=prod moneytransfer-*.jar &"
 exit 0
