4fbd301fdab93f5cc3931ffc33b83dc39ef39396
Lorenzo Gagliani <lorenz_g@hotmail.it>
2019-10-29 20:10:40 +0100

    fix run .jar file on deploy travis and exit 0

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,3 @@
 rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/build/libs/ root@206.81.16.40:/opt/apps/money-transfer
 ssh root@206.81.16.40 "cd /opt/apps/money-transfer && java -jar -Dspring.profiles.active=prod moneytransfer-*.jar &"
+exit 0
