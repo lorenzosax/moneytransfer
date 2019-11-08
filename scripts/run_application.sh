7df82104b24174f1f1854d43e9aa9e436a279a92
Lorenzo Gagliani <lorenz_g@hotmail.it>
2019-11-08 10:39:15 +0100

    fix run script deploy

--- scripts/run_application.sh
+++ scripts/run_application.sh
@@ -1,1 +1,1 @@
-ssh root@206.81.16.40 "cd /opt/apps/money-transfer && nohup java -jar -Dspring.profiles.active=prod moneytransfer-*.jar &"
+ssh root@206.81.16.40 "cd /opt/apps/money-transfer && java -jar -Dspring.profiles.active=prod moneytransfer-*.jar &"
