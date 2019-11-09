a6c642d13e5ee9114117cabc61aea300a3acca86
Lorenzo Gagliani <lorenz_g@hotmail.it>
2019-11-09 17:40:16 +0100

    angular get baseServerHost from Environment file

--- moneytransfer-ui/src/app/utils/constant.ts
+++ moneytransfer-ui/src/app/utils/constant.ts
@@ -1,6 +1,8 @@
+import { environment } from '../../environments/environment';
+
 export class Constant {
 
-  public static readonly BASE_SERVER_HOST = 'http://localhost:8080';
+  public static readonly BASE_SERVER_HOST = environment.baseServerHost;
   public static readonly ENDPOINT = {
     PREPARE_BONIFICO: Constant.BASE_SERVER_HOST + '/private/cliente/:customerId/conto/:accountNumber/bonifico/prepare',
     VERIFY_BONIFICO: Constant.BASE_SERVER_HOST + '/private/cliente/:customerId/conto/:accountNumber/bonifico/verify',
