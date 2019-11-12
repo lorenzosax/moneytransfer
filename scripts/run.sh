f48a9dc1cff98bdf6216cf546941ac0f98b280f2
Lorenzo Gagliani <lorenz_g@hotmail.it>
2019-11-12 23:47:59 +0100

    fix path tomcat

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,7 +3,7 @@
 ssh root@206.81.16.40 <<EOF
     sudo systemctl stop tomcat
     sleep 10
-    cd $CATALINA_HOME/webapps
+    cd /opt/tomcat/webapps
     rm -R money*
     cp /opt/apps/money-transfer/moneytransfer.war .
     sudo systemctl start tomcat
