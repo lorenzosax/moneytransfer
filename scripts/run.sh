25fd1c07d7c3399b08f19700ea119e44797dff7f
Lorenzo Gagliani <lorenz_g@hotmail.it>
2019-11-14 23:57:45 +0100

    fix deploy script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 ssh root@206.81.16.40 <<EOF
-  cd /opt/apps/money-transfer
-  java -jar -Dspring.profiles.active=prod moneytransfer*.jar &
+  sudo systemctl restart moneytransfer
 EOF
