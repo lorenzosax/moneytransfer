d7877a6c1a9d29e4e5b792a22bde545bc2a0881c
Lorenzo Gagliani <lorenz_g@hotmail.it>
2019-11-14 19:08:31 +0100

    return to produce jar

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 ssh root@206.81.16.40 <<EOF
-    sudo systemctl stop tomcat
-    sleep 10
-    cd /opt/tomcat/webapps
-    rm -R money*
-    cp /opt/apps/money-transfer/moneytransfer.war .
-    sudo systemctl start tomcat
+  cd /opt/apps/money-transfer
+  java -jar -Dspring.profiles.active=prod moneytransfer*.jar &
 EOF
